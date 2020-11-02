84f0a264e3516fb48c6fe35f6622541b9301aff8
jhonatanborg <jhonatanborgesdj@gmail.com>
2020-11-02 13:36:44 -0400

    pwa configs

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,12 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
   pwa: {
+    name: "Sabor Vital",
     themeColor: "#156f72",
-    msTileColor: "#000000",
+    msTileColor: "#FFFFFF",
+    manifestOptions: {
+      background_color: "#FFFFFF",
+    },
     iconPaths: {
       msTileImage: "img/icons/mstile-150x150.png",
     },
